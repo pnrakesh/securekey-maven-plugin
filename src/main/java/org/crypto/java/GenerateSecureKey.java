03972b151a0f04ba32dabe1bacbd1e5889052deb
Rakesh Pawar <rakesh.pawar@citrix.com>
2017-08-10 15:22:00 +0530

    snapshot version

---
+++ src/main/java/org/crypto/java/GenerateSecureKey.java
@@ -0,0 +1,125 @@
+package org.crypto.java;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.crypto.util.Util;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Properties;
+import java.util.UUID;
+
+/**
+ * Create a {@link sun.security.ssl.SecureKey} instance and generate the Base64 encoded string
+ * The secret to create SecureKey instance is passed as a parameter in the plugin configuration
+ *
+ * @goal generate
+ * @phase complile
+ */
+@Mojo(name = "securekey")
+public class GenerateSecureKey extends AbstractMojo {
+
+    /**
+     * @parameter
+     */
+    private Integer keySize;
+
+    /**
+     * @parameter
+     */
+    private String algorithm;
+
+    /**
+     * @parameter default-value="mojo-secret"
+     */
+    private String secret;
+
+    /**
+     * @parameter default-value="securekey.properties"
+     */
+    private String fileName;
+
+    /**
+     * @parameter
+     */
+    private String filePath;
+
+    /**
+     * @parameter default-value="secure.key.encoded"
+     */
+    private String propertyName;
+
+    public void execute() throws MojoExecutionException {
+
+        if (algorithm == null) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("Mention algorithm to use to generate a key in the configuration, for example:\n");
+            builder.append("<configuration>\n" +
+                    "<keySize>256</keySize><!-- optional, default is 128 bits-->\n" +
+                    "<algorithm>AES</algorithm>\n" +
+                    "<secret>secret</secret><!-- optional, default = random string -->"+
+                    "<fileName>key.properties</fileName><!-- optional, default = securekey.properties -->"+
+                    "<filePath>/path/to/file</filePath><!-- optional, default is project root-->"+
+                    "<propertyName>property.name</propertyName><!-- optional, default = secure.key.encoded -->"+
+                    "</configuration>");
+            throw new MojoExecutionException(builder.toString());
+        }
+
+
+        try {
+            System.out.println("********************************************************************");
+
+            System.out.println("keysize = " + keySize);
+            System.out.println("algorithm = " + algorithm);
+            SecretKey key = generateKey();
+            String encodedKey = Util.encodeAESKeyToBase64(key);
+            writeKeystringToFile(encodedKey);
+        } catch (Exception e) {
+            throw new MojoExecutionException(e.getMessage());
+        }
+        System.out.println("********************************************************************");
+
+    }
+
+    private SecretKey generateKey() throws UnsupportedEncodingException {
+        try {
+            final KeyGenerator keyGen = KeyGenerator.getInstance(algorithm);
+            SecureRandom random = new SecureRandom((secret+ UUID.randomUUID().toString()).getBytes(StandardCharsets.UTF_8.name()));
+            keyGen.init(keySize, random);
+            return keyGen.generateKey();
+        } catch (final NoSuchAlgorithmException e) {
+            // AES functionality is a requirement for any Java SE runtime
+            throw new IllegalStateException("AES should always be present in a Java SE runtime", e);
+        }
+    }
+
+    private void writeKeystringToFile(String keyString) throws IOException {
+        File file = new File(filePath+File.separator+fileName);
+        System.out.println("writing contents to file : " + file.getAbsolutePath());
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        Properties properties = new Properties();
+        properties.put(propertyName, keyString);
+        properties.store(writer, "");
+    }
+}
---
+++ src/main/java/org/crypto/util/Util.java
@@ -0,0 +1,23 @@
+package org.crypto.util;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+
+public class Util {
+    /**
+     * Encode a {@link SecretKey} object into base64 encoded string
+     * @param aesKey - Secret key
+     * @return - base64 encoded string
+     * @throws IllegalArgumentException
+     */
+    public static String encodeAESKeyToBase64(final SecretKey aesKey)
+            throws IllegalArgumentException {
+        if (!aesKey.getAlgorithm().equalsIgnoreCase("AES")) {
+            throw new IllegalArgumentException("Not an AES key");
+        }
+
+        final byte[] keyData = aesKey.getEncoded();
+        final String encodedKey = Base64.getEncoder().encodeToString(keyData);
+        return encodedKey;
+    }
+}
